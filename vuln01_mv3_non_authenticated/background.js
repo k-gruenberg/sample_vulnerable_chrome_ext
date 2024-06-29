a2d02924e99ecfe62e98cfd6ddf814a866fae877
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-06-29 12:39:22 +0200

    vuln01_mv3_ill_authenticated/

--- vuln01_mv3_non_authenticated/background.js
+++ vuln01_mv3_non_authenticated/background.js
@@ -7,7 +7,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // VULNERABILITY LIES HERE: Does not authenticate the URL (sender.url) at all
   //   => requesting "content script" may be from a malicious site that used a vulnerability in Chrome's rendering engine to escalate its privileges!
   //   => under the assumption of a regular (non-renderer) web attacker, this is not(!) a vulnerability as the service worker cannot receive
-  //      any messages from websites if the "externally_connectable" key isn't specified in the manifest.json (which it isn't in this case)!
+  //      any messages from websites if the "externally_connectable" key isn't specified in the manifest.json (which it isn't in this case,
+  //      and, besides, "chrome.runtime.onMessageExternal.addListener()" would have to be used instead, too)!
   chrome.cookies.getAll({}, // <= privileged API; see also: https://developer.chrome.com/docs/extensions/reference/api/cookies
     function(cookies) {
       //console.log(cookies);
