6e99e81371c54f80c013a3e808f5916eb67d447c
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-08-03 23:34:02 +0200

    bugfix

--- vuln02/vuln02_mv3_executeScript/background.js
+++ vuln02/vuln02_mv3_executeScript/background.js
@@ -9,26 +9,30 @@ chrome.runtime.onInstalled.addListener(() => {
 // More precisely, this service worker takes a value out of the extension storage and pastes it into other websites, unsanitized, resulting in UXSS:
 
 chrome.storage.local.get(["user_name"]).then((result) => {
-    let user_name = result["user_name"];
+    let user_name = "" + result["user_name"];
     console.log("Service worker retrieved user name from extension storage: " + user_name);
 
-    // In each open tab...
     chrome.tabs.query({}, // empty query => returns all tabs (active and inactive) of all windows
-        (tab) => { // => callback function for each tab
+        (tabs) => { // => callback function
 
-            // ...execute a script...
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                func: (uname) => { // => "This function will be serialized, and then deserialized for injection." (https://developer.chrome.com/docs/)
-                    
-                    // ...pasting the user name value from the extension storage into the content/DOM of the tab, unsanitized:
-                    const body = document.getElementsByTagName('body')[0];
-                    const new_span = document.createElement('span');
-                    new_span.innerHTML = 'Hello ' + uname + '!'; // <== VULNERABILITY LIES HERE! new_span.innerText should be used instead!
-                    body.append(new_span);
+            // In each open tab...
+            tabs.forEach((tab) => {
 
-                },
-                args: [user_name] // => arguments passed to the above function
+                // ...execute a script...
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    func: (uname) => { // => "This function will be serialized, and then deserialized for injection." (https://developer.chrome.com/docs/)
+                        
+                        // ...pasting the user name value from the extension storage into the content/DOM of the tab, unsanitized:
+                        const body = document.getElementsByTagName('body')[0];
+                        const new_span = document.createElement('span');
+                        new_span.innerHTML = 'Hello ' + uname + '!'; // <== VULNERABILITY LIES HERE! new_span.innerText should be used instead!
+                        body.append(new_span);
+
+                    },
+                    args: [user_name] // => arguments passed to the above function
+                });
+
             });
 
         }
