49f064341dcd51f71bdbb68d5b792250d83a1e0e
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-08-22 16:11:55 +0200

    added non_vuln01_mv3_well_authenticated_RETURN variant of non_vuln01_mv3_well_authenticated

---
+++ vuln01/non_vuln01_mv3_well_authenticated_RETURN/background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Extension installed');
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (sender.url !== "https://www.google.com/") {
+    return;
+  }
+  
+  console.log('URL authentication succeeded for ' + sender.url);
+  chrome.cookies.getAll({}, // <= privileged API; see also: https://developer.chrome.com/docs/extensions/reference/api/cookies
+    function(cookies) {
+      //console.log(cookies);
+      sendResponse(cookies);
+    }
+  );
+
+  // Return true to indicate that we will respond asynchronously!
+  return true;
+});
